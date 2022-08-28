412ab3d53096f7631e002ad53388abdee4038a05
saida <smussakhojayeva@gmail.com>
2022-08-28 20:34:06 +0600

    fix recognition script

--- recognize.py
+++ recognize.py
@@ -3,32 +3,12 @@ import argparse
 import numpy as np
 import wave, time, os
 
-asr_model_path="exp/asr_train_ksc2_raw_ksc2_char_sp"
-lm_model_path="exp/lm_train_lm_ksc2_char"
-wav_file='path_to_wav_to_be_recognized'
-
-train_config=asr_model_path + "/config.yaml"
-model_file=asr_model_path + "/valid.acc.ave.pth"
-
-lm_config = lm_model_path + "/config.yaml"
-lm_file = lm_model_path + "/valid.loss.ave.pth"
-
-speech2text = Speech2Text(
-    asr_train_config=train_config,
-    asr_model_file=model_file,
-    lm_train_config=lm_config,
-    lm_file=lm_file,
-    token_type=None,
-    bpemodel=None,
-    maxlenratio=0.0,
-    minlenratio=0.0,
-    beam_size=10,
-    ctc_weight=0.5,
-    lm_weight=0.3,
-    penalty=0.0,
-    nbest=1,
-    device = "cpu"
-)
+def get_args():
+    parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--wav_path", help="path to wav audio", required=True)
+    print(' '.join(sys.argv))
+    args = parser.parse_args()
+    return 
 
 def recognize(wavfile):
     timer = time.perf_counter()
@@ -46,4 +26,34 @@ def recognize(wavfile):
     print("RECOGNIZED", results[0][0])
     return results[0][0]
 
-recognize(wav_file)
+
+if __name__ == "__main__":
+    args = get_args()
+    wav_file = args.wav_path
+    if os.path.exists(wav_file): 
+        asr_model_path="exp/asr_train_ksc2_raw_ksc2_char_sp"
+        lm_model_path="exp/lm_train_lm_ksc2_char"
+
+        train_config=asr_model_path + "/config.yaml"
+        model_file=asr_model_path + "/valid.acc.ave.pth"
+
+        lm_config = lm_model_path + "/config.yaml"
+        lm_file = lm_model_path + "/valid.loss.ave.pth"
+
+        speech2text = Speech2Text(
+            asr_train_config=train_config,
+            asr_model_file=model_file,
+            lm_train_config=lm_config,
+            lm_file=lm_file,
+            token_type=None,
+            bpemodel=None,
+            maxlenratio=0.0,
+            minlenratio=0.0,
+            beam_size=10,
+            ctc_weight=0.5,
+            lm_weight=0.3,
+            penalty=0.0,
+            nbest=1,
+            device = "cpu"
+        )
+        recognize(wav_file)
